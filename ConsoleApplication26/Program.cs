1d6adf49b3b04f6cb38f6e0576b7a2d0a611ab9e
Piotrek <@>
2017-10-25 18:39:02 +0200

    pierwszy commit

---
+++ ConsoleApplication26/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Samochod auto = new Samochod();
+            auto.Model = "dfdsfs";
+            auto.Cena = 34;
+            auto.Marka = "dsdfd33";
+            Console.WriteLine("podaj cene");
+            string wejscie = Console.ReadLine();
+            double.TryParse(wejscie, out auto.pojemnoscSilnika);
+            
+        }
+    }
+}
---
+++ ConsoleApplication26/Samochod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication26
+{
+    class Samochod
+    {
+        private string model;
+        public double pojemnoscSilnika;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        private double cena;
+
+        public double Cena
+        {
+            get { return cena; }
+            set { cena = value; }
+        }
+
+        public string Marka { get; set; }
+        
+    }
+}
